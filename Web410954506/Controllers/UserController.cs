3d01e0e05752127b6ddae70ef0bc38fc522e0acf
stanley <stanleytwn@gmail.com>
2022-05-12 20:56:28 +0800

    0512signup

---
+++ Web410954506/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410954506.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult SignUp(string name, string account, string password)
+        {
+            if (name == null || name == "")
+            {
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            if (account == null || account == "")
+            {
+                ViewBag.AccountMessage = "請輸入帳號";
+            }
+            if (password == null || password == "")
+            {
+                ViewBag.PasswordMessage = "請輸入密碼";
+            }
+            if (name != null && account != null && password != null)
+            {
+                ViewBag.Message = "註冊成功";
+            }
+
+            return View();
+        }
+    }
+}
