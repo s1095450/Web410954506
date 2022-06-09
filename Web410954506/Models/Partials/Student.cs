89ceefe04c38c4bf4a5e8b94c05cb746816ae65f
stanley <stanleytwn@gmail.com>
2022-06-09 20:29:42 +0800

    0609

--- Web410954506/Models/Partials/Student.cs
+++ Web410954506/Models/Partials/Student.cs
@@ -20,7 +20,7 @@ namespace Web410954506.Models
         [Required(ErrorMessage = "請填寫姓名")]
         [Display(Name = "姓名")]
         [StringLength(5,MinimumLength =2,ErrorMessage = "請輸入2~5個字")]
-        public string Name { get; set; }
+        public string Username { get; set; }
         
         [Required(ErrorMessage = "請填寫學號")]
         [Display(Name = "學號")]
@@ -31,5 +31,9 @@ namespace Web410954506.Models
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "必填欄位")]
+        [Display(Name = "是否註冊")]
+        public bool IsActive { get; set; }
     }
 }
--- Web410954506/Models/Student.cs
+++ Web410954506/Models/Student.cs
@@ -15,8 +15,9 @@ namespace Web410954506.Models
     public partial class Student
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Username { get; set; }
         public string Number { get; set; }
         public string Email { get; set; }
+        public bool IsActive { get; set; }
     }
 }
